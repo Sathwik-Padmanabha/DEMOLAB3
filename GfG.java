92d25f0387a2419c8d4a3bd5f99421544d496f35
Sathwik-Padmanabha <151289571+Sathwik-Padmanabha@users.noreply.github.com>
2025-03-03 15:37:52 +0530

    Create GfG.java

---
+++ GfG.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class GfG {
+    static float findArea(float r) {
+        return (float)(Math.PI * r * r);
+    }
+
+    public static void main(String[] args) {
+        float r = 5;
+        float area = findArea(r);
+        System.out.printf("%.5f%n",area);
+    }
+}
